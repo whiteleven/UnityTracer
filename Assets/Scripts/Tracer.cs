4da9f5fefc455770452f375e345c9d126f634d49
whiteleven <is.whiteknight@gmail.com>
2016-03-07 23:37:36 +0900

    Add Tracer.cs

---
+++ Assets/Scripts/Tracer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tracer {
+    public const string Delimiter = ",";
+
+    public static void Print(params object[] args) {
+        string str = "";
+        foreach(object arg in args) {
+            str+=arg+Delimiter;
+        }
+        UnityEngine.Debug.Log(str.Substring(0, str.Length-1));
+    }
+
+    public static void PrintLine(params object[] args) {
+        foreach(object arg in args) {
+            UnityEngine.Debug.Log(arg.ToString());
+        }
+    }
+
+    public static void PrintRaw(params object[] args) {
+        string str = "";
+        foreach(object arg in args) {
+            str+=arg;
+        }
+        UnityEngine.Debug.Log(str);
+    }
+}
