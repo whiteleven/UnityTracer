58f4d99b9539237227ac7d8163eca99f7ed5f32b
whiteleven <is.whiteknight@gmail.com>
2016-03-08 22:39:31 +0900

    Write logs on a text file

--- Assets/Scripts/Tracer.cs
+++ Assets/Scripts/Tracer.cs
@@ -1,28 +1,87 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
-public class Tracer {
+public class Tracer{
+    private const string directoryName = "./Trace/";
+    private const string fileName = "";
+    private const string extension = ".txt";
+
     public const string Delimiter = ",";
 
-    public static void Print(params object[] args) {
-        string str = "";
-        foreach(object arg in args) {
-            str+=arg+Delimiter;
+    private List<string> memory = new List<string>();
+    private StreamWriter writer = null;
+
+    public Tracer() {
+    }
+
+    ~Tracer() {
+        if(!Directory.Exists(directoryName)) {
+            Directory.CreateDirectory(directoryName);
         }
-        UnityEngine.Debug.Log(str.Substring(0, str.Length-1));
+        using(StreamWriter writer = new StreamWriter(directoryName + getDateString() + fileName + extension)) {
+            foreach(string str in memory) {
+                writer.Write(str);
+            }
+        }
+    }
+
+    public static string GetLog(params object[] args) {
+        string log = "";
+        foreach(object arg in args) {
+            log += arg + Delimiter;
+        }
+        return log.Substring(0, log.Length - 1);
+    }
+
+    public static string GetLogRaw(params object[] args) {
+        string log = "";
+        foreach(object arg in args) {
+            log += arg;
+        }
+        return log;
+    }
+
+    public void Trace(params object[] args) {
+        string log = GetLog(args);
+        memory.Add(log + Environment.NewLine);
+        Debug.Log(log);
+    }
+
+    public void TraceLine(params object[] args) {
+        foreach(object arg in args) {
+            memory.Add(arg.ToString() + Environment.NewLine);
+            Debug.Log(arg.ToString());
+        }
+    }
+
+    public void TraceRaw(params object[] args) {
+        string log = GetLogRaw(args);
+        memory.Add(log);
+        Debug.Log(log);
+    }
+
+    public void NewLine() {
+        memory.Add(Environment.NewLine);
+    }
+
+    public static void Print(params object[] args) {
+        Debug.Log(GetLog(args));
     }
 
     public static void PrintLine(params object[] args) {
         foreach(object arg in args) {
-            UnityEngine.Debug.Log(arg.ToString());
+            Debug.Log(arg.ToString());
         }
     }
 
     public static void PrintRaw(params object[] args) {
-        string str = "";
-        foreach(object arg in args) {
-            str+=arg;
-        }
-        UnityEngine.Debug.Log(str);
+        Debug.Log(GetLogRaw(args));
+    }
+
+    private string getDateString() {
+        DateTime date = DateTime.Now;
+        return date.Year.ToString() + date.Month.ToString("00") + date.Day.ToString("00") + date.Hour.ToString("00") + date.Minute.ToString("00") + date.Second.ToString("00");
     }
 }
